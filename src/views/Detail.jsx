0324f28076a7473f16cf384e648916751dd4cf79
FloezeTv <mathematikschlumpf@gmail.com>
2020-03-21 23:34:37 +0100

    Redid ui and added example data

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Fab from '@material-ui/core/Fab';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import { Typography } from '@material-ui/core';
+import { Event } from '../model/model';
+import LinearProgress from '@material-ui/core/LinearProgress';
+//import FirebaseConnector from "../model/FirebaseConnector";
 
 
 const containerClass = "detailview container";
@@ -13,10 +18,64 @@ const mainnameClass = "detailview mainname";
 const timeClass = "detailview time";
 const nameClass = "detailview name";
 const descClass = "detailview description";
+const checkboxClass = "detailview checkbox";
+const sliderNotReachedClass = "detailview sliderNotReached";
+const sliderReachedClass = "detailview sliderReached";
+
+const checkedBox = <Checkbox checked />;
+const uncheckedBox = <Checkbox />;
+
+const exev = new Event(
+    "HGFVBNJKTFGHDMNBGYJK",
+    "MegaEvent",
+    { min: 0, max: 12 },
+    true,
+    "2020-03-20T17:00:00.00+01:00",
+    "This is a example category",
+    { price: "0", mandatory: false },
+    { blind: false, deaf: true, physicalDisabled: false, educationallyDisabled: false },
+    "Example Institution",
+    false,
+    "src/logo",
+    { min: 7, max: 10 },
+    "2020-04-20T17:00:00.00+01:00",
+    "Beste Kunstauffstellung der welt",
+    false,
+    [{
+        name: "hans",
+        email: "mail@mail.de"
+    }, {
+        name: "hans",
+        email: "mail@mail.de"
+    }, {
+        name: "hans",
+        email: "mail@mail.de"
+    }, {
+        name: "hans",
+        email: "mail@mail.de"
+    }, {
+        name: "hans",
+        email: "mail@mail.de"
+    }, {
+        name: "hans",
+        email: "mail@mail.de"
+    }]
+);
+
+const slider = (value, MIN, MAX) => (value - MIN) * 100 / (MAX - MIN);
 
 class Detail extends React.Component {
 
+
     render() {
+        //let event = new FirebaseConnector().getEvent(this.props.category, this.props.id);
+        let event = exev;
+        if (event == null) {
+            return (
+                <Typography className="error">Event nicht gefunden!</Typography>
+            );
+        }
+
         return (
             <>
                 <Fab size="small" color="secondary" aria-label="add" className={closeClass}>
@@ -25,38 +84,39 @@ class Detail extends React.Component {
                 <Grid container className={containerClass} spacing={0}>
                     <Grid item xs={12}>
                         <Paper className={titleClass}>
-                            <Typography className={mainnameClass}>{"Ggashlkw\nsGdhjsE\n" + this.props.category + "-" + this.props.id}</Typography>
-                            <Typography className={timeClass}>Montag, 13.März 2020{"\n"}20:50</Typography>
+                            <Typography className={mainnameClass}>{event.name}</Typography>
+                            <Typography className={mainnameClass}>{event.title}</Typography>
+                            <Typography className={timeClass}>{event.startDate}</Typography>
+                            <Typography className={timeClass}>{event.age.min + "-" + event.age.max + " Jahre (" + (event.childrenFriendly ? "" : "nicht ") + "geeignet für Kinder)"}</Typography>
+                            <Typography className={timeClass}>{event.financial.price + "€"}</Typography>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
                         <Paper className={itemClass}>
-                            <Typography className={nameClass}>Qui sunt incididunt non ullamco aute id aliqua cillum minim veniam.</Typography>
-                            <Typography className={descClass}>Anim adipisicing quis Lorem ex proident exercitation voluptate et id enim fugiat. Adipisicing magna proident culpa veniam duis aute adipisicing et exercitation. Qui ea enim proident laboris esse ex deserunt et labore proident eu. Adipisicing nostrud exercitation minim voluptate eiusmod. Pariatur exercitation enim reprehenderit ut sunt et nostrud non ut ipsum labore irure consequat irure. Id eu exercitation reprehenderit sint fugiat ea.</Typography>
+                            <Typography className={nameClass}>Beschreibung</Typography>
+                            <Typography className={descClass}>{event.description}</Typography>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
                         <Paper className={itemClass}>
-                            <Typography className={nameClass}>Aliquip anim cillum esse laborum esse nisi nostrud ex labore do voluptate laboris.</Typography>
-                            <Typography className={descClass}>Duis officia minim quis et dolore. Laboris excepteur sint velit aliquip voluptate pariatur magna ullamco amet anim ipsum pariatur occaecat. Irure tempor culpa in fugiat est veniam irure id exercitation. Eu anim veniam tempor eu adipisicing ea consequat laborum voluptate sint id mollit. Dolore et ad in id pariatur incididunt ad Lorem. Ipsum non amet occaecat dolore fugiat sit nisi commodo dolore velit eiusmod cupidatat Lorem tempor.</Typography>
+                            <Typography className={nameClass}>Teilnehmer: {event.members.length + "/" + event.memberCount.max}</Typography>
+                            <LinearProgress variant="buffer" className={event.members.length >= event.memberCount.min ? sliderReachedClass : sliderNotReachedClass} value={slider(event.members.length, 0, event.memberCount.max)} valueBuffer={slider(event.memberCount.min, 0, event.memberCount.max)} />
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
                         <Paper className={itemClass}>
-                            <Typography className={nameClass}>Proident culpa esse qui labore non voluptate veniam cupidatat qui labore adipisicing consectetur dolor et.</Typography>
-                            <Typography className={descClass}>Cillum mollit cupidatat ea amet incididunt aliqua enim deserunt et adipisicing sint occaecat. Ex aliquip dolore qui cillum sit ad aliqua nisi enim. Cupidatat minim adipisicing aute nulla ea dolor ad cupidatat id incididunt.</Typography>
+                            <FormControlLabel className={checkboxClass} disabled control={event.handicap.blind ? checkedBox : uncheckedBox} label="Blind" /><br />
+                            <FormControlLabel className={checkboxClass} disabled control={event.handicap.deaf ? checkedBox : uncheckedBox} label="Taub" /><br />
+                            <FormControlLabel className={checkboxClass} disabled control={event.handicap.educationallyDisabled ? checkedBox : uncheckedBox} label="educationally" /><br />
+                            <FormControlLabel className={checkboxClass} disabled control={event.handicap.physicalDisabled ? checkedBox : uncheckedBox} label="pyhsically" /><br />
+                            <FormControlLabel className={checkboxClass} disabled control={event.interactive ? checkedBox : uncheckedBox} label="interaktiv" /><br />
+                            <FormControlLabel className={checkboxClass} disabled control={event.together ? checkedBox : uncheckedBox} label="together" /><br />
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
                         <Paper className={itemClass}>
-                            <Typography className={nameClass}>Incididunt ad in amet cillum fugiat commodo enim deserunt aliquip consequat excepteur pariatur sunt.</Typography>
-                            <Typography className={descClass}>Et incididunt nostrud sunt nulla ad eiusmod ullamco anim culpa laboris non consequat. Aliqua exercitation id in ipsum sint do Lorem adipisicing et officia cupidatat nostrud ex. Voluptate dolore eu dolore amet sit.</Typography>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Paper className={itemClass}>
-                            <Typography className={nameClass}>Tempor nisi deserunt eu enim dolor irure et enim.</Typography>
-                            <Typography className={descClass}>Dolor exercitation cupidatat elit elit velit esse excepteur culpa sunt magna aute. Aliqua ut eu veniam culpa consectetur sunt consequat duis culpa. Officia do qui Lorem irure fugiat ex eu pariatur est magna ea non amet veniam. Sunt consectetur ullamco esse ut enim sint id proident eiusmod cillum cillum aliqua cillum id.</Typography>
+                            <Typography className={nameClass}>Institution</Typography>
+                            <Typography className={descClass}>{event.institution}</Typography>
                         </Paper>
                     </Grid>
                 </Grid>

2fe241bfb13f21fa715a6e36f954825eabc2dade
caroweng <caro.weng@gmx.net>
2020-03-22 15:19:45 +0100

    added ListView (expandable panels, filter)

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,34 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './views/Home';
 import Detail from './views/Detail';
-import SignUp from './views/SignUp';
 import './App.css';
+import ListView from "./views/listView/ListView";
 
 function Fallback() {
-  return <Redirect to='/' push />;
+    return <Redirect to='/' push />;
 }
 
 function App() {
-  return (
-    <Switch>
-      <Route
-        path='/'
-        component={Home}
-        exact
-      />
-      <Route path='/detailtest'
-        component={Detail}
-        exact
-      />
-      <Route 
-        path='/account/signup' 
-        component={SignUp}
-        exact
-        />
-      <Route
-        component={Fallback}
-        />
-    </Switch>
-  );
+    const LISTVIEW = (props) => <ListView categoryId={props.match.params.categoryId} history={props.history}/>;
+    return (
+        <Switch>
+            <Route
+                path='/'
+                component={Home}
+                exact
+            />
+            <Route path='/detailtest'
+                   component={Detail}
+                   exact
+            />
+            <Route path='/category/:categoryId'
+                   component={LISTVIEW}
+            />
+            <Route
+                component={Fallback}
+            />
+        </Switch>
+    );
 }
 
 export default App;
